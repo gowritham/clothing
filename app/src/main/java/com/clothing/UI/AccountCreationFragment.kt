443dac4187df987bb6480348654fef1044f7a3db
vsrinivasvemula <srinivasuday117@gmail.com>
2022-12-02 17:17:46 +0530

    register code

---
+++ app/src/main/java/com/clothing/UI/AccountCreationFragment.kt
@@ -0,0 +1,56 @@
+package eu.developer.sign_up
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.clothing.R
+import org.w3c.dom.Text
+
+
+class AccountCreationFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_account_creation, container, false)
+
+        val userName = view.findViewById<EditText>(R.id.Name)
+        val mailAddress = view.findViewById<EditText>(R.id.Email)
+        val password = view.findViewById<EditText>(R.id.password)
+        val button = view.findViewById<Button>(R.id.button_sign_up)
+
+        button.setOnClickListener{
+            if(userName.text.toString().isNotEmpty() && mailAddress.text.toString().isNotEmpty() && password.text.toString()
+                    .isNotEmpty()){
+                val k1 = Toast.makeText(
+                    activity,
+                    "Success",
+                    Toast.LENGTH_LONG
+                )
+                k1.show()
+
+
+            }else {
+                val k = Toast.makeText(
+                    activity,
+                    "Enter the input fields",
+                    Toast.LENGTH_LONG
+                )
+                k.show()
+            }
+        }
+        return view
+
+    }
+
+}
--- app/src/main/java/com/clothing/UI/LoginFragment.kt
+++ app/src/main/java/com/clothing/UI/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import com.clothing.R
 
 class LoginFragment : Fragment() {
@@ -15,7 +18,17 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+
+        val signUp = view.findViewById<TextView>(R.id.signUp)
+        signUp.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_accountCreationFragment)
+
+        }
         return view
+
+
+
 
 
 
