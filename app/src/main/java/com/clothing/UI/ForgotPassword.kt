2a3c3e648b25d00a26776ed430a9242020001012
gowritham <ggowtham@msystechnologies.com>
2022-12-27 16:15:08 +0530

    forgot screen Validation

--- app/src/main/java/com/clothing/UI/ForgotPassword.kt
+++ app/src/main/java/com/clothing/UI/ForgotPassword.kt
@@ -1,24 +1,56 @@
 package com.clothing.UI
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.clothing.R
+
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 
 class ForgotPassword : Fragment() {
+
+   // private lateinit var binding:FragmentForgotPasswordBinding
+    lateinit var  emailContainer:TextInputLayout
+    lateinit var userEmail:TextInputEditText
+    lateinit var submitBtn:Button
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+
+        val view=inflater.inflate(R.layout.fragment_forgot_password, container, false)
+
+        emailContainer=view.findViewById(R.id.emailContainer)
+        userEmail=view.findViewById(R.id.userEmail)
+        submitBtn=view.findViewById(R.id.submitBtn)
+
+       submitBtn.setOnClickListener {
+                  if(userEmail.text.toString().isEmpty()){
+                      emailContainer.helperText="Enter email"
+                  }else{
+
+
+                      val email=userEmail.text.toString()
+                      if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                          emailContainer.helperText = "Invalid Email"
+                          Toast.makeText(activity, "Failure", Toast.LENGTH_LONG).show()
+                      }else{
+                          emailContainer.helperText=null
+                          Toast.makeText(activity,"Correct", Toast.LENGTH_LONG).show()
+
+                      }
+                  }
+        }
+
+      return view
     }
-
-
-
 }
