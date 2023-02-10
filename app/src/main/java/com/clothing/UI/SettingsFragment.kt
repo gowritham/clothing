844b756fb58285851d44ce7b73d73af09f9d8b4d
PATNALA SAIKUMAR <saikumar.patnala9@gmail.com>
2023-02-10 11:53:21 +0530

    settings screen updation by mahesh

--- app/src/main/java/com/clothing/UI/SettingsFragment.kt
+++ app/src/main/java/com/clothing/UI/SettingsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.clothing.R
 
 
@@ -16,7 +19,16 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view =  inflater.inflate(R.layout.fragment_settings, container, false)
+        val faqNav = view.findViewById<ImageView>(R.id.faq_nav)
+        val psNav = view.findViewById<ImageView>(R.id.ps_nav)
+        faqNav.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.FAQFragment)
+        }
+        psNav.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.privacyFragment)
+        }
+        return view
     }
 
 }
